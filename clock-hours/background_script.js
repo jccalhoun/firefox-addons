84b94d8bf2051e7dbb995c648fac5ea403478c4d
jccalhoun <jccalhoun@users.noreply.github.com>
2019-01-30 22:49:39 -0500

    added code to change font color based on theme

--- clock-hours/background_script.js
+++ clock-hours/background_script.js
@@ -1,4 +1,35 @@
 //based on small clock https://github.com/davidillsley/small-clock
+//color update code from https://github.com/mdn/webextensions-examples/tree/master/theme-integrated-sidebar
+var textColor;
+function setTextColor(theme) {
+  // colors.frame and colors.accentcolor are aliases
+  if (theme.colors && theme.colors.toolbar_text) {
+    textColor = theme.colors.toolbar_text;
+  } else {
+    textColor = "white";
+  }
+}
+
+// Set the element style when the extension page loads
+async function setInitialStyle() {
+  const theme = await browser.theme.getCurrent();
+  setTextColor(theme);
+}
+setInitialStyle();
+
+// Watch for theme updates
+browser.theme.onUpdated.addListener(async ({ theme, windowId }) => {
+  const sidebarWindow = await browser.windows.getCurrent();
+  /*
+    Only update theme if it applies to the window the sidebar is in.
+    If a windowId is passed during an update, it means that the theme is applied to that specific window.
+    Otherwise, the theme is applied globally to all windows.
+  */
+  if (!windowId || windowId == sidebarWindow.id) {
+    setTextColor(theme);
+  }
+});
+
 function updateClock() {
 		var date = new Date();
 		var hours = date.getHours();
@@ -17,7 +48,7 @@ function updateClock() {
 
   var canvas = document.createElement("canvas");
   var context = canvas.getContext("2d");
-  context.fillStyle = "#FFFFFF";
+  context.fillStyle = textColor;
   context.font = "bold 100px sans-serif";
   context.fillText(hours +":", 0, 100);
   //context.font = "62px sans-serif";
